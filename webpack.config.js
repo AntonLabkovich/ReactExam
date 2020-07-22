2aad09190e90ba5c28e6558ddd44a727672ba994
AntonLabkovich <sharcnatch@gmail.com>
2020-07-22 18:55:36 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
             chunksSortMode: 'dependency'
         }),
         new MiniCssExtractPlugin({
-            filename:'form.css.css'
+            filename:'main.css'
         })
     ],
     module:{
